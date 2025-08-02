f164eb2fceaed85227561f4035a4e148734f08c8
Moses Rametsi <mosesrametsi16@gmail.com>
2025-08-02 21:55:21 +0200

    Add email validation, remove required attributes and changed the styles for the div element.

--- script.js
+++ script.js
@@ -5,36 +5,35 @@ const message = document.getElementById('message');
  
 form.addEventListener('submit', (event) => {
     event.preventDefault();
+
 })
 
 username.addEventListener('input', () => {
 
     const name = username.value;
 
-    if (name.length < 3) {
-        username.setCustomValidity('Username must be at least 3 characters long');
-    } else {
-        username.setCustomValidity('');
-    }
-
-    if (name.includes(' ')) {
-        username.setCustomValidity('Username cannot contain spaces');
-    } else {
-        username.setCustomValidity('');
-    }
-
-    if (name.length > 15) {
-        username.setCustomValidity('Username cannot exceed 15 characters');
-    } else {
-        username.setCustomValidity('');
-    }
-})
+     if (name.length < 3 || name.length > 15) {
+            message.innerHTML = 'Username must contain between 3 and 15 characters';
+        } else if (name.length > 15) {
+            message.innerHTML = 'Username cannot exceed 15 characters';
+        } else if (name.includes(' ')) {
+            message.innerHTML = 'Username cannot contain spaces';
+        } else {
+            message.innerHTML = '';
+        }
+});
 
 email.addEventListener('input', () => {
     const email = email.value;
-    const emailPattern = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+    // const emailPattern = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
 
-    if (!emailPattern.match(email)) {
-        email.setCustomValidity('Please enter a valid email address');   
+    if (email.excludes('@')) {
+        message.innerHTML = 'Please enter a valid email address';
+    } else if (email.length < 3 || email.length > 15) {
+        message.innerHTML = 'Email cannot be less than 3 or more than 15 characters';
+    } else if (email.includes(' ')) {
+        message.innerHTML = 'Email cannot contain spaces';
+    } else {
+        message.innerHTML = '';
     }
 })
