963ba2474a66e00e299e0e71ca9d9d90c3cc404f
Moses Rametsi <158572657+bblack-jewENT@users.noreply.github.com>
2025-08-08 01:16:49 +0200

    Update fetch-data.js
    Add try catch for error handling.

--- fetch-data.js
+++ fetch-data.js
@@ -1,7 +1,23 @@
 async function fetchUserData() {
     const apiUrl =  'https://jsonplaceholder.typicode.com/users';
-
     const dataContainer = document.getElementById('api-data');
+
+     try {
+        const response = await fetch(apiUrl);
+        const users = await response.json();
+        dataContainer.innerHTML = "";
+    
+        const userList = document.createElement("ul");
+        users.forEach((user) => {
+        const userListItem = document.createElement("li");
+        userListItem.textContent = user.name;
+        userList.appendChild(userListItem);
+    });
+        dataContainer.appendChild(userList);
+    } catch (error) {
+        dataContainer.innerHTML = "";
+        dataContainer.textContent = "Failed to load user data.";
+    }
 
    
     
